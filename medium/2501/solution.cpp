c903dc66e01810a7a12500d35ebc9a385c5b75a0
cichrrad <cichrrad@cvut.cz>
2025-08-26 20:38:12 +0200

    .

--- medium/2501/solution.cpp
+++ medium/2501/solution.cpp
@@ -5,23 +5,20 @@ public:
         set<long long> tmp(nums.begin(),nums.end());
                
         int best_chain = 1;
-        long long max = 3037000499LL;
         
         for(auto &el : tmp){
+            
             int c = 1;
             long long idx =el*el;
-            while(true){
-                if(tmp.contains(idx)){
-                    c++;
-                    idx = idx*idx;
-                }
-                else{
-                    best_chain = (best_chain > c ? best_chain : c);
-                    break;
-                }
+            while(tmp.count(idx)){
+                c++;
+                idx = idx*idx;
+
             }
+            
+            best_chain = (best_chain > c ? best_chain : c);
         }
 
-        return (best_chain > 1? best_chain : -1);
+        return (best_chain > 1 ? best_chain : -1);
     }
 };
